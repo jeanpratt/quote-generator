6dc3070b2d6e279dbf7cf2cc475ac7f17b92b63c
Jean Pratt <jean.dorothy.pratt@gmail.com>
2018-12-31 17:25:06 -0800

    Updates

---
+++ precache-manifest.72e7c93ffc8ed9ff8310f33a6eb4c376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7bcde82a9941da43acc",
+    "url": "/quote-generator/static/css/main.4246b46e.chunk.css"
+  },
+  {
+    "revision": "c7bcde82a9941da43acc",
+    "url": "/quote-generator/static/js/main.c7bcde82.chunk.js"
+  },
+  {
+    "revision": "52d9dde20b9e580b5bcf",
+    "url": "/quote-generator/static/js/1.52d9dde2.chunk.js"
+  },
+  {
+    "revision": "affdd53c48a26bdadeea",
+    "url": "/quote-generator/static/js/runtime~main.affdd53c.js"
+  },
+  {
+    "revision": "187043a5153f70745270d391b4a20a88",
+    "url": "/quote-generator/index.html"
+  }
+];
